576dc8d5bca7e4cbd2b0ddb64e71dab40992ac56
otavio brito <otaviob@otavios-MacBook-Pro.local>
2025-10-21 00:17:00 +1100

    URL instead in the RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,7 +11,7 @@ class RemoteFeedLoader {
     let client: HTTPClient
     let url: URL
     
-    init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
@@ -37,8 +37,9 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-give-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
