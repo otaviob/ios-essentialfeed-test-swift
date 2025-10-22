c220219b4e775cd614619202d8e6a85530f01834
otavio brito <otaviob@otavios-MacBook-Pro.local>
2025-10-22 18:22:29 +1100

    RemoteFeedLoader Public final class

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by otavio brito on 22/10/2025.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+    
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -1,31 +1,12 @@
 //
-//  Untitled.swift
+//  RemoteFeedLoaderTests3.swift
 //  EssentialFeed
 //
 //  Created by otavio brito on 16/10/2025.
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-    
-}
-
+@testable import EssentialFeed
 
 
 class RemoteFeedLoaderTests: XCTestCase {
